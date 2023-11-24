7d84ea3f649d77150651d395aa66b39d8e9ac643
Oleg Golovkov <oleg.golovkov@applifting.cz>
2023-11-24 20:00:45 +0100

    added: navigation animation

--- app/src/main/java/eu/golovkov/nba/MainActivity.kt
+++ app/src/main/java/eu/golovkov/nba/MainActivity.kt
@@ -3,7 +3,15 @@ package eu.golovkov.nba
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.runtime.Composable
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import eu.golovkov.core.designsystem.theme.NBATheme
 import eu.golovkov.core.navigation.RootNavGraph
 import eu.golovkov.core.network.di.NetworkModule
@@ -14,6 +22,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialNavigationApi::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,10 +38,30 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             NBATheme {
+                val engine = rememberAnimatedNavHostEngine(
+                    rootDefaultAnimations = createNavGraphAnimations(),
+                )
                 DestinationsNavHost(
-                    navGraph = RootNavGraph
+                    navGraph = RootNavGraph,
+                    engine = engine,
                 )
             }
         }
     }
 }
+
+@Composable
+private fun createNavGraphAnimations() = RootNavGraphDefaultAnimations(
+    enterTransition = {
+        slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Left) + fadeIn()
+    },
+    exitTransition = {
+        slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Left) + fadeOut()
+    },
+    popEnterTransition = {
+        slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Right) + fadeIn()
+    },
+    popExitTransition = {
+        slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Right) + fadeOut()
+    },
+)
