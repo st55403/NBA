dc39d710abc13da56dfe6671ec48b8efa13fd5cb
Oleg Golovkov <oleg.golovkov@applifting.cz>
2023-11-25 22:03:51 +0100

    changed Items inner parent element to card

--- feature/playerlist/src/main/java/eu/golovkov/feature/playerlist/PlayerListScreen.kt
+++ feature/playerlist/src/main/java/eu/golovkov/feature/playerlist/PlayerListScreen.kt
@@ -1,22 +1,19 @@
 package eu.golovkov.feature.playerlist
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
@@ -128,33 +125,28 @@ fun PlayerItem(
     user: Player,
     onClick: () -> Unit
 ) {
-    Row(
+    Card(
         modifier = modifier
             .fillMaxWidth()
-            .clickable { onClick() }
-            .background(
-                color = Color.Green,
-                shape = RoundedCornerShape(16.dp)
-            )
+            .clickable { onClick() },
+        shape = RoundedCornerShape(16.dp)
     ) {
-        Column {
-            Text(
-                modifier = modifier.padding(horizontal = 32.dp),
-                text = user.firstName ?: ""
-            )
-            Text(
-                modifier = modifier.padding(horizontal = 32.dp),
-                text = user.lastName ?: ""
-            )
-            Text(
-                modifier = modifier.padding(horizontal = 32.dp),
-                text = user.position ?: ""
-            )
-            Text(
-                modifier = modifier.padding(horizontal = 32.dp),
-                text = user.team.fullName ?: ""
-            )
-        }
+        Text(
+            modifier = modifier.padding(horizontal = 32.dp),
+            text = user.firstName ?: ""
+        )
+        Text(
+            modifier = modifier.padding(horizontal = 32.dp),
+            text = user.lastName ?: ""
+        )
+        Text(
+            modifier = modifier.padding(horizontal = 32.dp),
+            text = user.position ?: ""
+        )
+        Text(
+            modifier = modifier.padding(horizontal = 32.dp),
+            text = user.team.fullName ?: ""
+        )
     }
 }
 
